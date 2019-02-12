26f94497c3caa23a0310624d6bd23b64e58d2a2f
Robin Liu <Robin.Liu.831@gmail.com>
2019-02-11 21:14:27 -0500

    Fancy Casting (#1)
    * Casting lots hardcore mode
    
    * added readme
    
    * updated readme
    
    * readability
    
    * readme update
    
    * removed pycache
    
    * added .gitignore
    
    * updated .gitignore
    
    * addressed PR review
    
    * added optional countdown
    
    * neatlots responds to changes in number of lots
    
    * neatness

---
+++ README.md
@@ -0,0 +1,16 @@
+# cast-lots
+This module allows you to cast lots, where the lots are ASCII sticks.
+Interpretation, however, is up for interpretation.
+
+## Usage
+`from lots import Lots`
+
+Initialize the Lots class with a certain number of lots (default 10, positive integers only)
+
+`my_cast = Lots(20)`
+
+Cast the lots with `Lots.cast()`
+
+## Authors 
+- Jacob Deery
+- Robin Liu
--- lots.py
+++ lots.py
@@ -1,18 +1,96 @@
 import random
+import numpy as np
+import time
 
 
-possible_castings = ['|', '/', '\\', '_', '-']
+# Keys store casting possibilities
+# First list entry is the cost in lots (how many lots to construct the cast)
+# Second list entry is the relative probability
+# Purposely omitted: V, ^, N, W, T, L, H, A, [, ]
+castings = {
+    '|': [1, 1],
+    '/': [1, 1],
+    '\\': [1, 1],
+    '_': [1, 1],
+    '-': [1, 1],
+    '<': [2, 0.15],
+    '>': [2, 0.15],
+    '+': [2, 0.1],
+    '=': [2, 0.1],
+    'X': [2, 0.1],
+    'Z': [3, 0.02],
+    'Y': [3, 0.02],
+    'E': [4, 0.02],
+    '#': [4, 0.001],
+}
+
+# Normalize probabilities
+probabilities = [castings[cast][1] for cast in castings]
+probabilities = [i / sum(probabilities) for i in probabilities]
+
 
 class Lots:
-    def __init__(self, num_lots=5):
+    def __init__(self, num_lots=10):
+        if num_lots < 1 or not isinstance(num_lots, int):
+            raise ValueError('number of lots must be positive integer')
         self.num_lots = num_lots
-        self.lots = ['|'] * self.num_lots
-    
-    def cast(self):
-        self.lots = [random.choice(possible_castings) for i in range(self.num_lots)]
+        self.neat_lots = '|' * self.num_lots
+        print('\nHere are your lots:\n' + self.neat_lots + '\n')
+
+    def cast(self, countdown=0):
+        # Casts the lots, with an optional countdown in seconds
+        if countdown < 0:
+            raise ValueError('countdown time must be at least 0')
+
+        self.lots = self._decidelots()
+        grid_side = self._gridsize()
+
+        # Add an appropriate number of empty grid slots
+        self.lots += [' '] * (grid_side ** 2 - len(self.lots))
+        random.shuffle(self.lots)
+
+        # Display the lots
+        if countdown > 0:
+            print('casting in', countdown, 'seconds...')
+            time.sleep(countdown % 1)
+            countdown = int(countdown)
+            while countdown >= 1:
+                print(str(countdown) + '...')
+                time.sleep(1)
+                countdown -= 1
+            print()
+        for i in range(grid_side):
+            print("".join(self.lots[(i * grid_side):(i * grid_side + grid_side)]))
 
     def uncast(self):
-        self.lots = ['|'] * self.num_lots
+        self.neat_lots = '|' * self.num_lots
+        print('\n' + self.neat_lots + '\n')
 
-    def print_lots(self):
-        print("".join(self.lots))
+    def _decidelots(self):
+        # Decide which lot shapes to use, picking until there are no lots left
+        # Returns a list of the chosen lot shapes
+        lots = []
+        remaining_lots = self.num_lots
+        while remaining_lots > 0:
+            possible_castings = list(castings.keys())
+            cast = np.random.choice(possible_castings, p=probabilities)
+            remaining_lots -= castings[cast][0]
+            if remaining_lots >= 0:  # Enough lots remaining
+                lots.append(cast)
+            else:  # Not enough lots remaining
+                remaining_lots += castings[cast][0]
+        return lots
+
+    def _gridsize(self):
+        # Decide how big the (square) grid should be
+        # Returns the side length of the grid
+
+        # At least 2/3 of the spaces will be empty
+        singleside = int(np.ceil(np.sqrt(self.num_lots * 3)))
+        return singleside
+
+
+if __name__ == '__main__':
+    test = Lots()
+    test.cast(countdown=5)
+    test.uncast()
